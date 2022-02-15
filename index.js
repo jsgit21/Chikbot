177db505c5a12257c809eb941cbb689b17b62e87
Joseph Santantonio <js958@njit.edu>
2022-02-14 21:37:42 -0500

    adjusted wordle reset time

--- index.js
+++ index.js
@@ -64,10 +64,10 @@ let count = 0;
 
 function msTillWordleReset() {
     var wordleReset = new Date();
-    wordleReset.setHours(24,0,0,0);
+    wordleReset.setHours(0,0,0,0);
     var timeNow = new Date().getTime()
     var offsetMs
-    if (wordleReset < timeNow) {
+    if (wordleReset > timeNow) {
         wordleReset.setDate(wordleReset.getDate() + 1);
     }
     offsetMs = wordleReset - timeNow;
