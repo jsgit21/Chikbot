2194d286b1510f76601de9c4e97fdf85e50e9d9c
Joseph Santantonio <js958@njit.edu>
2022-02-20 16:46:42 -0500

    adjusting channel_id constants export

--- channel_id.js
+++ channel_id.js
@@ -1,11 +1,4 @@
-const wordlechat = "940720249454608414";
-const chikbot = "941770101672267808";
-const personalbottest = "941756769154252820";
-const mtgchat = "945070026015268924";
-
-export {
-    wordlechat,
-    chikbot,
-    personalbottest,
-    mtgchat
-}
+module.exports.wordlechat = "940720249454608414";
+module.exports.chikbot = "941770101672267808";
+module.exports.personalbottest = "941756769154252820";
+module.exports.mtgchat = "945070026015268924";
